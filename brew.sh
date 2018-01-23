fe9e6930027e46a0690f8f24a9648cf27e307601
ImpSy <sebastien.maintrot@contentsquare.com>
2018-01-23 13:46:51 +0100

    add vscode config

--- brew.sh
+++ brew.sh
@@ -23,8 +23,11 @@ pip install --upgrade pipenv
 pip install --upgrade ansible
 pip install --upgrade boto3
 pip install --upgrade awscli
+pip install --upgrade flake8
 
 code --install-extension ms-python.python
 code --install-extension robertohuertasm.vscode-icons
 code --install-extension lukehoban.go
 code --install-extension vscodevim.vim
+
+cp $PWD/settings.json $HOME/Library/Application\ Support/Code/User/
