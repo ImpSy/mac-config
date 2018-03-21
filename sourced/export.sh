97b8607112d77001652cd24c856b664bf4f52e32
ImpSy <sebastien.maintrot@contentsquare.com>
2018-03-21 13:28:21 +0100

    add new env vars

--- sourced/export.sh
+++ sourced/export.sh
@@ -1,1 +1,6 @@
-export PATH=/usr/local/opt/python/libexec/bin:$PATH
+PYTHON_BIN_PATH="$(python2 -m site --user-base)/bin"
+export PATH=$PYTHON_BIN_PATH:$PATH
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export GOPATH=$HOME/go
+export PATH=$PATH:$(go env GOPATH)/bin
