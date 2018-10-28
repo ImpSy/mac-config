4a4377b3305124cb9b47733de4548645e0086b45
ImpSy <sebastien.maintrot@gmail.com>
2018-10-28 12:00:18 +0100

    update vs-code plugins

--- vs-code.sh
+++ vs-code.sh
@@ -1,8 +1,11 @@
+code --install-extension eamodio.gitlens
+code --install-extension mauve.terraform
+code --install-extension ms-ceintl.vscode-language-pack-fr
 code --install-extension ms-python.python
-code --install-extension robertohuertasm.vscode-icons
-code --install-extension lukehoban.go
-code --install-extension vscodevim.vim
-code --install-extension shardulm94.trailing-spaces
+code --install-extension ms-vscode.go
 code --install-extension PeterJausovec.vscode-docker
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension shardulm94.trailing-spaces
+code --install-extension vscodevim.vim
 
 cp $PWD/settings.json $HOME/Library/Application\ Support/Code/User/
