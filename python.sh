7c99db1f5fafadf0d0fa6c40bf7833f85f09b399
ImpSy <sebastien.maintrot@dolead.com>
2019-12-20 15:36:30 +0100

    new 2020 version

--- python.sh
+++ python.sh
@@ -1,21 +1,14 @@
 which pyenv
 
-PYTHON2_VERSION="2.7.15"
-PYTHON3_VERSION="3.7.0"
+PYTHON_VERSION="3.7.5"
 
-pyenv versions | grep $PYTHON2_VERSION || pyenv install $PYTHON2_VERSION
-pyenv versions | grep $PYTHON3_VERSION || pyenv install $PYTHON3_VERSION
+pyenv versions | grep $PYTHON_VERSION || pyenv install $PYTHON_VERSION
 
-(pyenv global | grep PYTHON2_VERSION && pyenv global | grep PYTHON3_VERSION) || pyenv global $PYTHON2_VERSION $PYTHON3_VERSION
+pyenv global $PYTHON_VERSION
 
 eval "$(pyenv init -)"
 
 which pip
 which pip3
 
-pip install --upgrade pip
-pip3 install --upgrade pip
-
-pip install --upgrade virtualenv
-pip3 install --upgrade virtualenv
-pip install --upgrade awscli
+pip install pipx
